87dd5929b840bbf2177e8f825055a17633d2ed1c
Bence Csizmadia <b.csizmadia@synoa.de>
2023-06-19 10:06:53 +0200

    fix: strict types

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,7 +4,9 @@ import "classes.dart";
 import "package:yaml/yaml.dart";
 
 const String configFile = "dart_hooks.yaml";
-final Iterable keys = loadYaml(File(configFile).readAsStringSync()).keys;
+final Iterable<String> keys =
+    (loadYaml(File(configFile).readAsStringSync()).keys as Iterable)
+        .map((dynamic key) => key.toString());
 
 File config = File(configFile);
 Logger logger = Logger();
