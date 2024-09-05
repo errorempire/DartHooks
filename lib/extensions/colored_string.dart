c1510bfee5bc1126c784b64a619220806212668b
Bence Csizmadia <bencecsizmadia@icloud.com>
2024-09-05 16:48:39 +0200

    misc(): add magenta color

--- lib/extensions/colored_string.dart
+++ lib/extensions/colored_string.dart
@@ -4,9 +4,11 @@ extension ColoredStringExtension on String {
   static const _blue = "\x1B[34m";
   static const _yellow = "\x1B[33m";
   static const _reset = "\x1B[0m";
+  static const _magenta = "\x1B[35m";
 
   String get blue => '$_blue$this$_reset';
   String get green => '$_green$this$_reset';
+  String get magenta => '$_magenta$this$_reset';
   String get red => '$_red$this$_reset';
   String get yellow => '$_yellow$this$_reset';
 }
